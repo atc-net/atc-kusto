59cf16e57e6da9de792201e75cf285dccb3044d6
Per Kops <pko@delegate.dk>
2025-01-09 08:20:16 +0100

    test: add unit-tests for SimpleQueryHandler

---
+++ test/Atc.Kusto.Tests/Handlers/Internal/SimpleQueryHandlerTests.cs
@@ -0,0 +1,96 @@
+namespace Atc.Kusto.Tests.Handlers.Internal;
+
+public sealed class SimpleQueryHandlerTests
+{
+    [Theory, AutoNSubstituteData]
+    internal async Task Execute_ShouldReturnResult_WhenQueryExecutesSuccessfully(
+        [Frozen] ICslQueryProvider queryProvider,
+        [Frozen] IKustoQuery<string> query,
+        [Frozen] IDataReader reader,
+        SimpleQueryHandler<string> sut,
+        string queryText,
+        Dictionary<string, object> parameters,
+        string expectedResult,
+        CancellationToken cancellationToken)
+    {
+        // Arrange
+        query.GetQueryText().Returns(queryText);
+        query.GetParameters().Returns(parameters);
+
+        queryProvider
+            .ExecuteQueryAsync(
+                databaseName: default,
+                query.GetQueryText(),
+                query.GetClientRequestProperties(),
+                cancellationToken)
+            .ReturnsForAnyArgs(reader);
+
+        query
+            .ReadResult(reader)
+            .ReturnsForAnyArgs(expectedResult);
+
+        // Act
+        var result = await sut.Execute(cancellationToken);
+
+        // Assert
+        result
+            .Should()
+            .Be(expectedResult);
+
+        await queryProvider
+            .Received(1)
+            .ExecuteQueryAsync(
+                databaseName: default,
+                query.GetQueryText(),
+                Arg.Is<ClientRequestProperties>(p
+                        => p.ClientRequestId != null &&
+                           p.Parameters.SequenceEqual(query.GetCslParameters())),
+                cancellationToken);
+
+        query
+            .Received(1)
+            .ReadResult(reader);
+    }
+
+    [Theory, AutoNSubstituteData]
+    internal async Task Execute_ShouldReturnNull_WhenQueryResultIsNull(
+        [Frozen] ICslQueryProvider queryProvider,
+        [Frozen] IKustoQuery<string?> query,
+        [Frozen] IDataReader reader,
+        SimpleQueryHandler<string?> sut,
+        CancellationToken cancellationToken)
+    {
+        // Arrange
+        queryProvider
+            .ExecuteQueryAsync(
+                databaseName: default,
+                query.GetQueryText(),
+                query.GetClientRequestProperties(),
+                cancellationToken)
+            .ReturnsForAnyArgs(reader);
+
+        query
+            .ReadResult(reader)
+            .ReturnsForAnyArgs((string?)null);
+
+        // Act
+        var result = await sut.Execute(cancellationToken);
+
+        // Assert
+        Assert.Null(result);
+
+        await queryProvider
+            .Received(1)
+            .ExecuteQueryAsync(
+                databaseName: default,
+                query.GetQueryText(),
+                Arg.Is<ClientRequestProperties>(p
+                    => p.ClientRequestId != null &&
+                       p.Parameters.SequenceEqual(query.GetCslParameters())),
+                cancellationToken);
+
+        query
+            .Received(1)
+            .ReadResult(reader);
+    }
+}
