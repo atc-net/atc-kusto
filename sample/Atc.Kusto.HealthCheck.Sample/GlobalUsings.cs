d6884ae4dac759a4ec107c6aaf4089372f92c284
Per Kops <pko@delegate.dk>
2025-06-23 13:02:46 +0200

    feat(sample): add sample for healthcheck usage

---
+++ sample/Atc.Kusto.HealthCheck.Sample/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Linq;
+global using System.Text;
+global using System.Text.Json;
+global using Atc.Kusto;
+global using Atc.Kusto.HealthChecks;
+global using Azure.Identity;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
