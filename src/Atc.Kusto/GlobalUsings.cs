f9920c41def80cae3c38f7e4d1961c721f7eef6a
Per Kops <per_kops@hotmail.com>
2025-09-14 12:25:38 +0200

    feat(serialization): add decimal converter and update serializer docs

--- src/Atc.Kusto/GlobalUsings.cs
+++ src/Atc.Kusto/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using System.Collections.Concurrent;
 global using System.Data;
+global using System.Data.SqlTypes;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Reflection;
 global using System.Runtime.CompilerServices;
