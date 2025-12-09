1ad54140b536daec237cd0df0b6e621c255484db
Per Kops <pko@delegate.dk>
2025-12-09 21:18:36 +0100

    fix(logging): include query text and errors in SemanticException logs

--- src/Atc.Kusto/Handlers/Internal/SimpleQueryHandlerLoggerMessages.cs
+++ src/Atc.Kusto/Handlers/Internal/SimpleQueryHandlerLoggerMessages.cs
@@ -7,7 +7,7 @@ internal sealed partial class SimpleQueryHandler<T>
     [LoggerMessage(
         EventId = LoggingEventIdConstants.SimpleQueryHandler.KustoServicePartialQueryFailureException,
         Level = LogLevel.Error,
-        Message = "An partial query failure exception occured for clientRequestId {ClientRequestId} and query {Query}")]
+        Message = "A partial query failure exception occurred for clientRequestId {ClientRequestId} and query {Query}")]
     private partial void LogKustoServicePartialQueryFailureException(
         Exception ex,
         string clientRequestId,
@@ -16,7 +16,7 @@ internal sealed partial class SimpleQueryHandler<T>
     [LoggerMessage(
         EventId = LoggingEventIdConstants.SimpleQueryHandler.KustoServiceException,
         Level = LogLevel.Error,
-        Message = "An kusto service exception occured for clientRequestId {ClientRequestId}")]
+        Message = "A Kusto service exception occurred for clientRequestId {ClientRequestId}")]
     private partial void LogKustoServiceException(
         Exception ex,
         string clientRequestId);
@@ -24,7 +24,7 @@ internal sealed partial class SimpleQueryHandler<T>
     [LoggerMessage(
         EventId = LoggingEventIdConstants.SimpleQueryHandler.SemanticException,
         Level = LogLevel.Error,
-        Message = "An semantic exception occured for clientRequestId {ClientRequestId}")]
+        Message = "A semantic exception occurred for clientRequestId {ClientRequestId}. Query: {Text}. Errors: {SemanticErrors}")]
     private partial void LogSemanticException(
         Exception ex,
         string clientRequestId,
@@ -34,6 +34,6 @@ internal sealed partial class SimpleQueryHandler<T>
     [LoggerMessage(
         EventId = LoggingEventIdConstants.SimpleQueryHandler.UnhandledException,
         Level = LogLevel.Error,
-        Message = "An unhandled exception occured and some data might be lost")]
+        Message = "An unhandled exception occurred and some data might be lost")]
     private partial void LogUnhandledException(Exception ex);
 }
