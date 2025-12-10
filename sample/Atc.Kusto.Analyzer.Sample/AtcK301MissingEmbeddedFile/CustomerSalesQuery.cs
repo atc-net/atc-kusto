acf7f0c1b9d8224f819791d62a7e8d81283cc3b3
Per Kops <pko@delegate.dk>
2025-12-10 11:02:45 +0100

    chore(sample): rename CustomerSalesQuery

--- sample/Atc.Kusto.Analyzer.Sample/AtcK301MissingEmbeddedFile/CustomerSalesQuery.cs
+++ sample/Atc.Kusto.Analyzer.Sample/AtcK301MissingEmbeddedFile/CustomerSalesQuery.cs
@@ -1,7 +1,7 @@
 // Test: Should report ATCK301 - Missing .kusto file
-#pragma warning disable ATCK301
+////#pragma warning disable ATCK301
 
 namespace Atc.Kusto.Analyzer.Sample.AtcK301MissingEmbeddedFile;
 
-public record CustomerSalesQuery2
+public record CustomerSalesQuery
     : KustoQuery<CustomerSales>;
